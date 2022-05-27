1c0b933b2796eadcbdba4deb323f6760aa00f1b4
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-05-28 02:37:38 +0500

    Adding Material UI

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const Login = () => {
   return (
-    <div>
-        Login
-    </div>
+    <Container>
+        <Grid 
+            style={{height: window.innerHeight - 50}}
+            container 
+            alignItems="center" 
+            justifyContent="center"
+        >
+            <Grid
+                style={{width: 400, background: 'rgba(0, 0, 0, 0.07)'}}
+                container
+                alignItems="center"
+                direction="colimn"
+                justifyContent="center"
+            >
+                <Box p={5}>
+                    <Button variant="outlined">Log in with google</Button>
+                </Box>
+            </Grid>
+        </Grid>
+    </Container>
   )
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
 
 const Navbar = () => {
+  const user = true;
+
   return (
     <div>
-        Navbar
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Messanger
+                </Typography>
+                {   user ?
+
+                    <Button color="inherit" variant="outlined">
+                        Sign out
+                    </Button>  
+                    
+                    :
+
+                    <NavLink to={LOGIN_ROUTE} >
+                        <Button color="inherit" variant="outlined">
+                            Login
+                        </Button>
+                    </NavLink>
+                    
+                }
+                </Toolbar>
+            </AppBar>
+        </Box>
     </div>
   )
 }
